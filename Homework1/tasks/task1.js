b8d0ff64872d4c20ca44425df820e52415d0e3e4
Jangaiah Paneti <Jangaiah_Paneti@epam.com>
2020-07-12 12:51:43 +0530

    Task1 completed

--- Homework1/tasks/task1.js
+++ Homework1/tasks/task1.js
@@ -1,1 +1,9 @@
-console.log("------------ Task1 -----------");
+console.log("------------ Task1: String reverse -----------");
+
+process.stdin.setEncoding('utf8');
+process.stdin.on('readable', () => {
+    let chunk;
+    while ((chunk = process.stdin.read()) !== null) {
+      process.stdout.write(`${chunk.replace(/\n/,'').split('').reverse().join('')}\n\n`);
+    }
+  });
